262bc04ff53d7b76c0859690c3f4c350f9dc82e9
Oskar <oskar.otwinowski@gmail.com>
2025-06-06 04:16:58 +0200

    fix Redis pw/username

--- apps/server/src/api/api.module.ts
+++ apps/server/src/api/api.module.ts
@@ -14,8 +14,8 @@ import { RailwayClientService } from '../railway-client/services/railway-client.
       provide: 'pubsub',
       useFactory: () => {
         return require('mqemitter-redis')({
-          ...(process.env.REDIS_USER ? {user: process.env.REDIS_USER} : {}),
-          ...(process.env.REDIS_PASSWORD ? {user: process.env.REDIS_PASSWORD} : {}),
+          ...(process.env.REDIS_USER ? {username: process.env.REDIS_USER} : {}),
+          ...(process.env.REDIS_PASSWORD ? {password: process.env.REDIS_PASSWORD} : {}),
           port: process.env.REDIS_PORT ? parseInt(process.env.REDIS_PORT, 10) : 6379,
           host: process.env.REDIS_HOST || 'localhost',
         });
